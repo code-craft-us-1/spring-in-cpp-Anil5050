9b261422ffade9bf9068929c182612f728fd2ed8
Anil5050 <anil.kumar_1@philips.com>
2024-08-28 11:47:55 -0700

    Update Test case for NAN

--- stats.cpp
+++ stats.cpp
@@ -4,19 +4,19 @@
 Stats Statistics::ComputeStatistics(const std::vector<float>& vec) {
     //Implement statistics here
     float sum = 0.0;
-    status.min = status.max = vec[0]; //{1.5, 8.9, 3.2, 4.5}
+    Stats.min = Stats.max = vec[0]; //{1.5, 8.9, 3.2, 4.5}
 
     for (int i = 1; i < vec.size() ; i++) {
 
-        if (vec[i] < status.min)
-            status.min = vec[i];
-        if (vec[i] > status.max)
-            status.max = vec[i];
+        if (vec[i] < Stats.min)
+            Stats.min = vec[i];
+        if (vec[i] > Stats.max)
+            Stats.max = vec[i];
 
         sum = sum + vec[i]+ vec[0];
     }
 
-    status.average = sum / vec.size();
+    Stats.average = sum / vec.size();
 
     return status;
 }
--- stats.h
+++ stats.h
@@ -1,11 +1,12 @@
 #include <vector>
 
-struct Stats
+struct 
 {
     float average;
     float min;
     float max;
-}status;
+}Stats;
+
 
 namespace Statistics {
     // define the Stats structure here. See the tests to infer its properties
