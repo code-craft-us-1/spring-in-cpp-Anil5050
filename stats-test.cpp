be955851fe9c489b9616fd405b60736538caabb7
Anil5050 <anil.kumar_1@philips.com>
2024-08-28 12:24:54 -0700

    Update Test case for NAN

--- stats-test.cpp
+++ stats-test.cpp
@@ -3,16 +3,16 @@
 #include <cmath>
 using namespace  Statistics;
 
-TEST(Statistics, ReportsAverageMinMax) {
-    auto computedStats = Statistics::ComputeStatistics({1.5, 8.9, 3.2, 4.5});
-    float epsilon = 0.001;
-    EXPECT_LT(std::abs(computedStats.average - 4.525), epsilon);
-    EXPECT_LT(std::abs(computedStats.max - 8.9), epsilon);
-    EXPECT_LT(std::abs(computedStats.min - 1.5), epsilon);
-}
+//TEST(Statistics, ReportsAverageMinMax) {
+//    auto computedStats = Statistics::ComputeStatistics({1.5, 8.9, 3.2, 4.5});
+//    float epsilon = 0.001;
+//    EXPECT_LT(std::abs(computedStats.average - 4.525), epsilon);
+//    EXPECT_LT(std::abs(computedStats.max - 8.9), epsilon);
+//    EXPECT_LT(std::abs(computedStats.min - 1.5), epsilon);
+//}
 
 TEST(Statistics, AverageNaNForEmpty) {
-    auto computedStats = Statistics::ComputeStatisticsNAN({ NAN, 1.5, 3.2, 4.5 });
+    auto computedStats = Statistics::ComputeStatistics({ NAN, 1.5, 3.2, 4.5 });
     float epsilon = 0.001;
 
     EXPECT_THAT(std::abs(computedStats.average - 4.525), epsilon);
--- stats.cpp
+++ stats.cpp
@@ -1,28 +1,28 @@
 #include "stats.h"
 
 
+//Stats Statistics::ComputeStatistics(const std::vector<float>& vec) {
+//    //Implement statistics here
+//    float sum = 0.0;
+//    Stats structObj;
+//    structObj.min = structObj.max = sum = vec[0]; //{1.5, 8.9, 3.2, 4.5}
+//
+//    for (int i = 1; i < vec.size(); i++) {
+//
+//        if (vec[i] < structObj.min)
+//            structObj.min = vec[i];
+//        if (vec[i] > structObj.max)
+//            structObj.max = vec[i];
+//
+//        sum = sum + vec[i];
+//    }
+//
+//    structObj.average = sum / vec.size();
+//
+//    return structObj;
+//}
+
 Stats Statistics::ComputeStatistics(const std::vector<float>& vec) {
-    //Implement statistics here
-    float sum = 0.0;
-    Stats structObj;
-    structObj.min = structObj.max = sum = vec[0]; //{1.5, 8.9, 3.2, 4.5}
-
-    for (int i = 1; i < vec.size(); i++) {
-
-        if (vec[i] < structObj.min)
-            structObj.min = vec[i];
-        if (vec[i] > structObj.max)
-            structObj.max = vec[i];
-
-        sum = sum + vec[i];
-    }
-
-    structObj.average = sum / vec.size();
-
-    return structObj;
-}
-
-Stats Statistics::ComputeStatisticsNAN(const std::vector<float>& vec) {
     //Implement statistics here
     float sum = 0.0;
     Stats structObj;
