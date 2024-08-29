fae42b84e20198f8dc0915f7c5013e65cdee9087
Anil5050 <anil.kumar_1@philips.com>
2024-08-29 02:26:08 -0700

    Update Test case for NAN

--- stats-test.cpp
+++ stats-test.cpp
@@ -14,9 +14,9 @@ TEST(Statistics, ReportsAverageMinMax) {
 TEST(Statistics, AverageNaNForEmpty) {
     auto computedStats = Statistics::ComputeStatistics({ NAN, 1.5, 8.9, 4.5 });
     
-    EXPECT_TRUE(isnan(computedStats.average));
-    EXPECT_TRUE(isnan(computedStats.min));
-    EXPECT_TRUE(isnan(computedStats.max));
+    EXPECT_TRUE(std::isnan(computedStats.average));
+    EXPECT_TRUE(std::isnan(computedStats.min));
+    EXPECT_TRUE(std::isnan(computedStats.max));
     //All fields of computedStats (average, max, min) must be
     //NAN (not-a-number), as defined in math.h
     //Design the gtest EXPECT statement here.
